56c893c28e04a778ab5b22bcfa003ecae6994a57
gassar0 <mmmmm_mmmmm319@yahoo.com>
2026-08-30 08:08:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 DB_Name = 'store.db'
 
-# إعدادات بوت تيليجرام
-  TELEGRAM_BOT_TOKEN = '8969435828:AAEsccn8O8KuiqaVLQSERnxY2rstA8SF8JQ'
-TELEGRAM_CHAT_ID = '8508616708'    
+# إعدادات بوت تيليجرام 
+TELEGRAM_BOT_TOKEN = '8969435828:AAEsccn8O8KuiqaVLQSERnxY2rstA8SF8JQ'
+TELEGRAM_CHAT_ID = '8508616708'
 
 def send_telegram_notification(message):
     if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
