efc9bf2137f2eb3655c682a16b031929969e51eb
gassar0 <mmmmm_mmmmm319@yahoo.com>
2026-09-05 10:56:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'smart_store_secret_key_12345'
+# إعدادات قاعدة البيانات السحابية (PostgreSQL) مع احتياطي محلي لو مش موجودة
+database_url = os.environ.get("DATABASE_URL")
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///store.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # ==========================================
 # ⚙️ إعدادات تيليجرام
